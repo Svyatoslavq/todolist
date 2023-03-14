3b910fa670827e0bb0e51196ccf10e77bb8618ef
Svyatoslavq <sadness0v2@gmail.com>
2023-03-14 19:54:45 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import Header from './components/Header';
+import TodoList from './components/TodoList';
+import AddTodo from './components/addTodo';
+import { useState } from 'react';
+import {Container} from 'react-bootstrap'
+import ReactDOM from 'react-dom/client';
+
+
+function App() {
+
+  const [todo, setTodo] = useState([
+    {
+      id: 1,
+      title: 'first todo',
+      status: true
+    },
+    {
+      id: 2,
+      title: 'second todo',
+      status: true
+    },
+    {
+      id: 3,
+      title: 'third todo',
+      status: false
+    },
+  ])
+  console.log(todo)
+
+  return (
+    <Container className="App" >
+        <Header/>
+        <AddTodo todo={todo} setTodo={setTodo}/>
+        <TodoList todo={todo} setTodo={setTodo}/>
+    </Container>
+  );
+}
+
+export default App;
