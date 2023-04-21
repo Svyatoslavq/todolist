574de9bc2c48268ac4d8a5605300ed94f635f696
Svyatoslavq <sadness0v2@gmail.com>
2023-04-21 20:20:22 +0300

    commit

--- src/components/addTodo/addTodo.js
+++ src/components/addTodo/addTodo.js
@@ -1,42 +1,36 @@
-import React, { useState } from "react";
-import { v1 as uuidv1 } from "uuid";
-import { Row, Col, Button, FormControl } from "react-bootstrap";
-import s from "./addTodo.module.css";
+import React, { useState } from 'react';
+import { Row, Col, Button, FormControl } from 'react-bootstrap';
+import s from './addTodo.module.css';
 
-function AddTodo({ todo, setTodo }) {
-  const [value, setValue] = useState("");
+const AddTodo = ({ todo, setTodo }) => {
+  const [value, setValue] = useState('');
 
-  function saveTodo() {
+  const saveTodo = () => {
     const newTodo = {
-      id: uuidv1(),
+      id: Date.now(),
       title: value,
       status: true,
     };
     setTodo([...todo, newTodo]);
-    setValue("");
+    setValue('');
 
-    localStorage.setItem("localTodo", JSON.stringify([...todo, newTodo]));
-  }
+    localStorage.setItem('localTodo', JSON.stringify([...todo, newTodo]));
+  };
   return (
     <Row>
       <Col className={s.addTodoForm}>
         <FormControl
           placeholder="Введите задачу"
-          onKeyPress={(e) => e.key === "Enter" && saveTodo()}
+          onKeyPress={(e) => e.key === 'Enter' && saveTodo()}
           value={value}
           onChange={(e) => setValue(e.target.value)}
-        />
-        <Button
-          onClick={saveTodo}
-          className={s.btn}
-          variant="success"
-          size="lg"
-        >
-          Сохранить
-        </Button>
-      </Col>
+        />{' '}
+        <Button onClick={saveTodo} className={s.btn} variant="success" size="lg">
+          Сохранить{' '}
+        </Button>{' '}
+      </Col>{' '}
     </Row>
   );
-}
+};
 
 export default AddTodo;
